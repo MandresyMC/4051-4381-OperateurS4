c36e42196bc9bbe39aee28ec578ea7986085e237
MMC <m.zanajatovo@gmail.com>
2026-07-20 11:50:48 +0300

    situation clientelle

---
+++ app/Controllers/Admin/SituationClientController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Models\OperationModel;
+
+class DashboardController extends BaseController
+{
+    protected $operationModel;
+
+    public function __construct()
+    {
+        $this->operationModel = new OperationModel();
+    }
+
+    public function index()
+    {
+        $sql = "
+            SELECT 
+                u.id,
+                u.numero_telephone,
+                u.solde,
+                COALESCE(SUM(o.montant), 0) AS total_operations
+            FROM user u
+            LEFT JOIN operation o ON o.id_user_source = u.id
+            GROUP BY u.id;
+        ";
+
+        $gains = $this->operationModel->query($sql)->getResult();
+
+        return view('admin/dashboard', ['gains' => $gains]);
+    }
+
+}
