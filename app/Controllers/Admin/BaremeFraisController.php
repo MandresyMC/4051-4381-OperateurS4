8bca31ffe95222060ac56fde1419b58a1c8fd84d
MMC <m.zanajatovo@gmail.com>
2026-07-20 14:36:42 +0300

    debug

---
+++ app/Controllers/Admin/BaremeFraisController.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace App\Controllers\Admin;
+
+class BaremeFraisController extends AdminBaremeFraisController
+{
+}
---
+++ app/Controllers/Client/OperationController.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace App\Controllers\Client;
+
+class OperationController extends ClientOperationController
+{
+}
